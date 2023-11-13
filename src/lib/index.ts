1a9a9e7e4900015a39227035b14ca18cca8534e0
crlsktr <crlsktr@gmail.com>
2023-11-12 23:01:09 -0700

    add list and button

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,10 @@
 // place files you want to import through the `$lib` alias in this folder.
+interface Gift{
+	giftNumber: number;
+	summary: string;
+	details: string;
+}
+
+export const presentList: Gift[] = [
+	{giftNumber:1, summary:"Girl socks", details: "She likes floral patterns and wears shoe size 6"}
+]
