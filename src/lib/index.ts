a812a77b2a4995309d86eb1cf65b23e27ec8610d
crlsktr <crlsktr@gmail.com>
2023-11-13 09:27:36 -0700

    move text to lib

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -8,3 +8,7 @@ interface Gift{
 export const presentList: Gift[] = [
 	{giftNumber:1, summary:"Girl socks", details: "She likes floral patterns and wears shoe size 6"}
 ]
+
+export const coordinatorPhoneNumber = "+18887776666";
+export const signupMessage = "Hi%21%20I%20want%20to%20help%20out%20with%20this%20gift%21%0AMy%20Name%3A%0AMy%20Phone%20Number%3A%0AGift%20Number%3A";
+export const welcomeMessage = "Help us get gifts for some families in the neighborhood";
