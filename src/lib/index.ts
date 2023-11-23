990785b73ea33588f445cb0fa9a9c3b9e66563b0
jorrigarcia <96459222+jorrigarcia@users.noreply.github.com>
2023-11-22 22:43:27 -0700

    Minor corrections to gift descriptions

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -8,13 +8,13 @@ interface Gift {
 export const presentList: Gift[] = [
 	{ giftNumber: 'B1', summary: "Book for 10 year old boy", details: 'He would like a nonfiction book and is interested in astronomy and animals'}
 	,{ giftNumber: 'B2', summary: 'Clothes for 10 year old boy', details: 'He needs shirts and Levis. He wears size 10 shirts and size 10 slim pants.'}
-	,{ giftNumber: 'B3', summary: 'Outfits for infant boy', details: 'He wears size 2T and needs winter outfits and PJ' }
+	,{ giftNumber: 'B3', summary: 'Outfits for infant boy', details: 'He wears size 2T and needs winter outfits and PJs' }
 	,{ giftNumber: 'B4', summary: 'Baby toy', details: 'Sensory or learning toy for baby 6-12 months' }
 	,{ giftNumber: 'C1', summary: 'Boys snow pants size 12/14', details: '' }
 	,{ giftNumber: 'C2', summary: 'Toy for 8-year-old boy', details: 'He likes Pokemon and Legos' }
-	,{ giftNumber: 'C3', summary: 'Girls snow pants size 6/6x', details: '' }
+	,{ giftNumber: 'C3', summary: 'Girls snow pants size 6/6x', details: 'This is listed twice for two different children. It\'s not a duplicate 😁' }
 	,{ giftNumber: 'C4', summary: 'Toy for 5-year-old girl', details: 'She likes Pokemon, puzzles, and Nerf' }
-	,{ giftNumber: 'C5', summary: 'Girls snow pants size 6/6x', details: '' }
+	,{ giftNumber: 'C5', summary: 'Girls snow pants size 6/6x', details: 'This is listed twice for two different children. It\'s not a duplicate 😁' }
 	,{ giftNumber: 'C6', summary: 'Toy for 5-year-old girl', details: 'She likes anything girly. She likes stuffed animals, baby dolls, and dress up.' }
 	,{ giftNumber: 'D1', summary: 'Toy for 8-year-old boy', details: 'He likes Fortnite, Minecraft and Harry Potter' }
 	,{ giftNumber: 'D2', summary: 'Clothes for 8-year-old boy', details: 'He wears size 5/6 boys and needs shirts and pants. His favorite color is green and he likes Harry Potter, Fortnite, and Minecraft' }
@@ -36,7 +36,7 @@ export const presentList: Gift[] = [
 	,{ giftNumber: 'E11', summary: 'Book "Under the Cover Mercy" by Rebecca Connolly OR Ravenclaw scarf and beanie', details: 'Please choose just one of the options for this gift' }
 	,{ giftNumber: 'E12', summary: 'Book "CNA Study Guide" by Dr. Andrew Stapleton OR full size sheet set', details: 'Please choose just one of the options for this gift' }
 	,{ giftNumber: 'E13', summary: 'Gift card to the movies OR Utah Jazz shirt size large', details: 'Please choose just one of the options for this gift' }
-	,{ giftNumber: 'E14', summary: 'Blue jeans size 32/34 slim OR new pillows for bed', details: 'Please choose just one of the options for this gift' }
+	,{ giftNumber: 'E14', summary: 'Blue jeans mens size 32/34 slim OR new pillows for bed', details: 'Please choose just one of the options for this gift' }
 	,{ giftNumber: 'E15', summary: 'Golden State Warriors beanie or hoodie size L OR temple recommend holder and gift card to Swig', details: 'Please choose just one of the options for this gift' }
 	,{ giftNumber: 'E16', summary: 'Tennis shoes size mens 12 wide OR pillows for bed ', details: 'Please choose just one of the options for this gift' }
 	,{ giftNumber: 'F1', summary: 'Size 6 Diapers', details: 'Huggies brand is preferred due to sensitive skin' }
